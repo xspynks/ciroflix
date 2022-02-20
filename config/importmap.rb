5517b64f6f3ad5a3beaf8183f7abc1576c2dfbdb
Rafael de Paula <xspynks@gmail.com>
2022-02-20 14:10:31 -0300

    feat: implementando carousel

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,6 +2,8 @@
 
 pin "application", preload: true
 pin "home", to: "home.js", preload: false
+pin "jquery.min.js", to: "jquery.min.js", preload: false
+pin "owl.carousel.min.js", to: "owl.carousel.min.js", preload: false
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
