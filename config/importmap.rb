cf6a1f8d14a87aaae2a3e772941ea38df5e7692d
Rafael de Paula <xspynks@gmail.com>
2022-02-20 02:46:04 -0300

    refact and security

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,9 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
+pin "jquery.min.js", to: "jquery.min.js", preload: true
+pin "owl.carousel.min.js", to: "owl.carousel.min.js", preload: true
+pin "setup.js", to: "setup.js", preload: true
 pin "home.js", to: "home.js", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
