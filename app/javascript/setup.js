8de4b30ead49b7cb45a1db3bf90fcdf0c5f6c75f
Rafael de Paula <xspynks@gmail.com>
2022-02-20 15:47:32 -0300

    feat: implementa o carousel

---
+++ app/javascript/setup.js
@@ -0,0 +1,22 @@
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 0,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: false,
+    },
+    600: {
+      items: 3,
+      nav: false,
+    },
+    1000: {
+      items: 5,
+      nav: true,
+      loop: true,
+      slideBy: 1,
+      dots: false,
+    },
+  },
+});
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,9 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
-pin "home", to: "controllers/home.js", preload: true
-pin "jquery.min.js", to: "controllers/jquery.min.js", preload: true
-pin "owl.carousel.min.js", to: "controllers/owl.carousel.min.js", preload: true
+pin "setup", to: "setup.js", preload: true
+pin "jquery.min.js", to: "jquery.min.js", preload: true
+pin "owl.carousel.min.js", to: "owl.carousel.min.js", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
