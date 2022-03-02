9c1feb482965ff03ef9d58d421f210b5a02bc1f1
Rafael de Paula <xspynks@gmail.com>
2022-03-01 22:46:12 -0300

    fix try

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,9 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
-pin "setup", to: "setup.js", preload: true
-pin "jquery.min.js", to: "jquery.min.js", preload: true
-pin "owl.carousel.min.js", to: "owl.carousel.min.js", preload: true
+pin "setup", to: "app/javascript/setup.js", preload: true
+pin "jquery.min.js", to: "app/javascript/jquery.min.js", preload: true
+pin "owl.carousel.min.js", to: "app/javascript/owl.carousel.min.js", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
