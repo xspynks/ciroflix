ed1dc195a7bcab05b1b223f1383c990e555be973
Rafael de Paula <xspynks@gmail.com>
2022-06-03 03:12:06 -0300

    fix: remove falha de autenticação

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,6 @@
 class PicturesController < ApplicationController
   before_action :set_picture, only: %i[ show edit update destroy ]
+  before_action :authenticate_admin!, except: %i[ index show ]
 
   # GET /pictures or /pictures.json
   def index
