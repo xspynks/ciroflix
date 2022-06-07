9d58e35c8c18ab1646f47858d1d3b1269bf8e098
Rafael de Paula <xspynks@gmail.com>
2022-06-07 18:24:08 -0300

    style: altera botoes do owl carousel

--- app/javascript/setup.js
+++ app/javascript/setup.js
@@ -1,8 +1,20 @@
 $(".owl-carousel").owlCarousel({
   loop: true,
-  nav: false,
-  autoplay: true,
-  autoplayHoverPause: true,
   margin: 0,
-  dots: true,
+  nav: true,
+  dots: false,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+      loop: true,
+      slideBy: 1,
+    },
+  },
 });
